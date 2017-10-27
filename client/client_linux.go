f2aa427af9bbf1a206e6ce097b61091f941499ae
Serge Voilokov <serge0x76@gmail.com>
2017-10-26 22:49:21 -0400

    Increase notification timeout

--- client/client_linux.go
+++ client/client_linux.go
@@ -6,7 +6,7 @@ import (
 )
 
 func notify(title, text string) {
-	err := exec.Command("notify-send", "chatc", text).Run()
+	err := exec.Command("notify-send", "-t", "60000", "chatc", text).Run()
 	if err != nil {
 		log.Println("notify-send", err)
 	}
