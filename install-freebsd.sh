133358dd4dccb4df3d258058ba12ae43a1d541d3
milla <milla@localhost>
2018-01-08 21:47:09 -0500

    Fix enable flag

--- install-freebsd.sh
+++ install-freebsd.sh
@@ -9,7 +9,7 @@ cd -
 sudo service chatd stop | head
 sudo cp cmd/chatd/chatd /usr/local/www/wet/chatd
 sudo cp chatd /usr/local/etc/rc.d
-grep chatd_enable /etc/rc.conf || sudo sh -c 'echo chatd_enable=\"YES\" >> /etc/rc.conf'
+grep chat_enable /etc/rc.conf || sudo sh -c 'echo chat_enable=\"YES\" >> /etc/rc.conf'
 sudo service chatd start
 sleep 3
 tail -20 /var/log/chatd.log
