90584fa1ad1d4ed069bfaeb24c9f936a271c4e3d
milla <milla@localhost>
2018-01-08 21:42:49 -0500

    Add install script

---
+++ install-freebsd.sh
@@ -0,0 +1,16 @@
+set -x
+set -e
+
+git tag 1 | head
+VER=`git describe --tags --long`
+cd cmd/chatd
+go build -ldflags "-X service.Version=$VER"
+cd -
+sudo service chatd stop | head
+sudo cp cmd/chatd/chatd /usr/local/www/wet/chatd
+sudo cp chatd /usr/local/etc/rc.d
+grep chatd_enable /etc/rc.conf || sudo sh -c 'echo chatd_enable=\"YES\" >> /etc/rc.conf'
+sudo service chatd start
+sleep 3
+tail -20 /var/log/chatd.log
+/usr/local/www/wet/chatd -version
