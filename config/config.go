0a8f9c5732e4e0f42f7e3c205ae6edda4b6c2f18
milla <milla@localhost>
2018-03-27 07:01:43 -0400

    Remove loading config

--- config/config.go
+++ config/config.go
@@ -2,9 +2,6 @@
 package config
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
 	"os"
 )
 
@@ -26,33 +23,4 @@ var Config = &ServiceConfig{
 	Address:  "wet." + hostname() + ":8085",
 	WorkDir:  "/usr/local/www/wet/work/",
 	CertPath: "/usr/local/etc/letsencrypt/golang-autocert",
-}
-
-var configFile = "/usr/local/etc/chatd.json"
-
-// LoadConfig loads custom or default config.
-func LoadConfig(fname string) {
-	if fname != "" {
-		configFile = fname
-	}
-
-	f, err := os.Open(configFile)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	dec := json.NewDecoder(f)
-	if err := dec.Decode(Config); err != nil {
-		panic(err)
-	}
-	log.Println(configFile, "config loaded")
-}
-
-// PrintConfig prints loaded config to stdout.
-func PrintConfig() {
-	fmt.Println("config file:", configFile)
-	fmt.Println("loaded config:")
-	enc := json.NewEncoder(os.Stdout)
-	enc.SetIndent("", "    ")
-	enc.Encode(Config)
 }
